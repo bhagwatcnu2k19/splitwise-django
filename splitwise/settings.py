16dde11defd176cef2a80f86170d29149f665374
Aditya Bhagwat <aditya@Adityas-MBP.mshome.net>
2019-08-12 02:28:53 +0400

    Added secret key to untracke config file

--- splitwise/settings.py
+++ splitwise/settings.py
@@ -30,7 +30,7 @@ db_config = yaml.load(open('splitwise/config.yaml'))['DATABASE']
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o(!@*wmo7472^a4%14n(ga-+k_h8=nxxmv20o!yu225i(r3q5a'
+SECRET_KEY = yaml.load(open('splitwise/config.yaml'))['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
